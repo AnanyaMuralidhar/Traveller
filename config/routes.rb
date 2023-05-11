6db11eadcc87695b6d1769798e9b0d9acd925cf1
Rajashri <rjshri293@gmail.com>
2023-05-11 12:35:26 +0530

    Added basic navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
    get 'home/index'
+   get 'home/about'
   # Defines the root path route ("/")
   # root "home#index"
   devise_scope :user do
